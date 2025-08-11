008437db4eb8c26c499d23152112d5a5642b76ef
Galih Wicaksono <galihwicaksono90@gmail.com>
2025-08-11 17:11:25 +0700

    update x11

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,9 @@
-if true then
-  return {}
-end
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    "MunifTanjim/nui.nvim",
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
   },
   cmd = 'Neotree',
